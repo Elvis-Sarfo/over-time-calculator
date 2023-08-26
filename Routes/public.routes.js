e8c9acd04ef6f1b3a3600105af281359f254ee83
Elvis Sarfo Antwi <sarfoantwielvis@gmail.com>
2023-08-26 12:21:32 +0000

    init node

---
+++ Routes/public.routes.js
@@ -0,0 +1,332 @@
+const public_routes = {};
+
+public_routes.admin_dashboard = "/index";
+
+public_routes.teacher_dashboard = "/teacher-dashboard";
+
+public_routes.student_dashboard = "/student-dashboard";
+
+public_routes.students = "/students";
+
+public_routes.students_grid = "/students-grid";
+
+public_routes.student_details = "/student-details";
+
+public_routes.add_student = "/add-student";
+
+public_routes.edit_student = "/edit-student";
+
+public_routes.teachers = "/teachers";
+
+public_routes.teachers_grid = "/teachers-grid";
+
+public_routes.teacher_details = "/teacher-details";
+
+public_routes.add_teacher = "/add-teacher";
+
+public_routes.edit_teacher = "/edit-teacher";
+
+public_routes.departments = "/departments";
+
+public_routes.add_department = "/add-department";
+
+public_routes.edit_department = "/edit-department";
+
+public_routes.subjects = "/subjects";
+
+public_routes.add_subject = "/add-subject";
+
+public_routes.edit_subject = "/edit-subject";
+
+public_routes.invoices = "/invoices";
+
+public_routes.invoices_cancelled = "/invoices-cancelled";
+
+public_routes.invoices_draft = "/invoices-draft";
+
+public_routes.invoices_overdue = "/invoices-overdue";
+
+public_routes.invoices_paid = "/invoices-paid";
+
+public_routes.invoices_recurring = "/invoices-recurring";
+
+public_routes.invoice_grid = "/invoice-grid";
+
+public_routes.add_invoice = "/add-invoice";
+
+public_routes.edit_invoice= "/edit-invoice";
+
+public_routes.view_invoice= "/view-invoice";
+
+public_routes.invoices_settings= "/invoices-settings";
+
+public_routes.tax_settings= "/tax-settings";
+
+public_routes.bank_settings= "/bank-settings";
+
+// ----------- ( Management ) ------------
+
+public_routes.fees_collections= "/fees-collections";
+
+public_routes.expenses= "/expenses";
+
+public_routes.salary= "/salary";
+
+public_routes.add_fees_collection= "/add-fees-collection";
+
+public_routes.add_expenses= "/add-expenses";
+
+public_routes.add_salary= "/add-salary";
+
+public_routes.holiday= "/holiday";
+
+public_routes.add_holiday= "/add-holiday";
+
+public_routes.fees= "/fees";
+
+public_routes.add_fees= "/add-fees";
+
+public_routes.edit_fees= "/edit-fees";
+
+public_routes.exam= "/exam";
+
+public_routes.add_exam= "/add-exam";
+
+public_routes.edit_exam= "/edit-exam";
+
+public_routes.event= "/event";
+
+public_routes.edit_events= "/edit-events";
+
+public_routes.add_events= "/add-events";
+
+public_routes.time_table= "/time-table";
+
+public_routes.add_time_table= "/add-time-table";
+
+public_routes.edit_time_table= "/edit-time-table";
+
+public_routes.library = "/library";
+
+public_routes.edit_books= "/edit-books";
+
+public_routes.add_books= "/add-books";
+
+public_routes.blog= "/blog";
+
+public_routes.blog_details= "/blog-details";
+
+public_routes.pending_blog= "/pending-blog";
+
+public_routes.add_blog= "/add-blog";
+
+public_routes.edit_blog= "/edit-blog";
+
+public_routes.blog_categories= "/blog-categories";
+
+public_routes.settings= "/settings";
+
+public_routes.localization_details= "/localization-details";
+
+public_routes.payment_settings= "/payment-settings";
+
+public_routes.email_settings= "/email-settings";
+
+public_routes.social_settings= "/social-settings";
+
+public_routes.social_links= "/social-links";
+
+public_routes.seo_settings= "/seo-settings";
+
+public_routes.others_settings= "/others-settings";
+
+// ------------- ( Pages ) ----------------
+
+public_routes.login = "/login";
+
+public_routes.register = "/register";
+
+public_routes.forgot_password = "/forgot-password";
+
+public_routes.blank_page = "/blank-page";
+
+// ------------- ( Others ) ----------------
+
+public_routes.sports = "/sports";
+
+public_routes.add_sports = "/add-sports";
+
+public_routes.edit_sports = "/edit-sports";
+
+public_routes.hostel = "/hostel";
+
+public_routes.edit_room = "/edit-room";
+
+public_routes.add_room = "/add-room";
+
+public_routes.transport = "/transport";
+
+public_routes.add_transport = "/add-transport";
+
+public_routes.edit_transport = "/edit-transport";
+
+// ------------ ( error page ) ----------------
+public_routes.pageNotFount = "/error-404";
+
+public_routes.serverError = "/error-500";
+
+// ----------- ( Base UI ) -------------------
+
+public_routes.alerts = "/alerts";
+
+public_routes.accordions = "/accordions";
+
+public_routes.avatar = "/avatar";
+
+public_routes.badges = "/badges";
+
+public_routes.buttons = "/buttons";
+
+public_routes.buttongroup = "/buttongroup";
+
+public_routes.breadcrumbs = "/breadcrumbs";
+
+public_routes.cards = "/cards";
+
+public_routes.carousel = "/carousel";
+
+public_routes.dropdowns = "/dropdowns";
+
+public_routes.grid = "/grid";
+
+public_routes.images = "/images";
+
+public_routes.lightbox = "/lightbox";
+
+public_routes.media = "/media";
+
+public_routes.modal = "/modal";
+
+public_routes.offcanvas = "/offcanvas";
+
+public_routes.pagination = "/pagination";
+
+public_routes.popover = "/popover";
+
+public_routes.progress = "/progress";
+
+public_routes.placeholders = "/placeholders";
+
+public_routes.rangeslider = "/rangeslider";
+
+public_routes.spinners = "/spinners";
+
+public_routes.sweetalerts = "/sweetalerts";
+
+public_routes.tab = "/tab";
+
+public_routes.toastr = "/toastr";
+
+public_routes.tooltip = "/tooltip";
+
+public_routes.typography = "/typography";
+
+public_routes.video = "/video";
+
+// ------- ( Elements ) ---------
+
+public_routes.ribbon = "/ribbon";
+
+public_routes.clipboard = "/clipboard";
+
+public_routes.drag_drop = "/drag-drop";
+
+public_routes.rating = "/rating";
+
+public_routes.text_editor = "/text-editor";
+
+public_routes.counter = "/counter";
+
+public_routes.scrollbar = "/scrollbar";
+
+public_routes.notification = "/notification";
+
+public_routes.stickynote = "/stickynote";
+
+public_routes.timeline = "/timeline";
+
+public_routes.horizontal_timeline = "/horizontal-timeline";
+
+public_routes.form_wizard = "/form-wizard";
+
+// ---------- (charts) ----------------
+
+public_routes.chart_apex = "/chart-apex";
+
+public_routes.chart_js = "/chart-js";
+
+public_routes.chart_morris = "/chart-morris";
+
+public_routes.chart_flot = "/chart-flot";
+
+public_routes.chart_peity = "/chart-peity";
+
+public_routes.chart_c3 = "/chart-c3";
+
+// ----------- ( Icons ) --------------
+
+public_routes.icon_fontawesome = "/icon-fontawesome";
+
+public_routes.icon_feather = "/icon-feather";
+
+public_routes.icon_ionic = "/icon-ionic";
+
+public_routes.icon_material = "/icon-material";
+
+public_routes.icon_pe7 = "/icon-pe7";
+
+public_routes.icon_simpleline = "/icon-simpleline";
+
+public_routes.icon_themify = "/icon-themify";
+
+public_routes.icon_weather = "/icon-weather";
+
+public_routes.icon_typicon = "/icon-typicon";
+
+public_routes.icon_flag = "/icon-flag";
+
+// --------------- ( Forms ) ----------------
+
+public_routes.form_basic_inputs = "/form-basic-inputs";
+
+public_routes.form_input_groups = "/form-input-groups";
+
+public_routes.form_horizontal = "/form-horizontal";
+
+public_routes.form_vertical = "/form-vertical";
+
+public_routes.form_mask = "/form-mask";
+
+public_routes.form_validation = "/form-validation";
+
+// ---------- ( Tables ) -----------------
+
+public_routes.tables_basic = "/tables-basic";
+
+public_routes.data_tables = "/data-tables";
+
+// ----------- ( Profile ) ------------------
+
+public_routes.compose = "/compose";
+
+public_routes.inbox = "/inbox";
+
+public_routes.profile = "/profile";
+
+public_routes.spinner = "/spinner";
+
+
+
+
+
+module.exports = public_routes;
